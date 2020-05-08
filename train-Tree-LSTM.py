added hidden units, fixed bugs

train-Tree-LSTM.py
@@ -6,20 +6,35 @@ from torch.utils.data import Dataset, IterableDataset, DataLoader
 
 import os
 import codecs
+from sklearn.metrics import f1_score
+import random
+import numpy as np
+
+seed_val = 12
+
+random.seed(seed_val)
+np.random.seed(seed_val)
+torch.manual_seed(seed_val)
+torch.cuda.manual_seed_all(seed_val)
+
 
 tree_path = '../../Dataset/Parsed-Trees/'
 
-test_set = ['charliehebdo.txt'] #, 'germanwings-crash.txt', 'ottawashooting.txt', 'sydneysiege.txt']
+test_set = ['charliehebdo.txt']
+
+from random import shuffle
 
 IN_FEATURES = 40
 OUT_FEATURES = 2
-NUM_ITERATIONS = 100
-BATCH_SIZE = 8
+NUM_ITERATIONS = 10
+BATCH_SIZE = 50
+HIDDEN_UNITS = 128
+LEARNING_RATE = 0.001
 
 files = os.listdir(tree_path)
 
+
 for test_file in test_set:
-	# print('Now Testing ', test_file)
 	print('Training Set:', set(files) - {test_file})
 
 	test_trees = []
@@ -29,6 +44,8 @@ for test_file in test_set:
 		input_file = codecs.open(tree_path + filename, 'r', 'utf-8')
 
 		tree_li = []
+		pos_trees = []
+		neg_trees = []
 
 		for row in input_file:
 			s = row.strip().split('\t')
@@ -37,27 +54,36 @@ for test_file in test_set:
 			curr_tree = eval(s[1])
 
 			try:
-
-				curr_tensor = convert_tree_to_tensors(curr_tree)
-
+				curr_tensor, curr_label = convert_tree_to_tensors(curr_tree)
 			except:
 				continue
 
-			tree_li.append(curr_tensor)
+			curr_tensor['tweet_id'] = tweet_id
+
+			if curr_label == 1:
+				pos_trees.append(curr_tensor)
+			else:
+				neg_trees.append(curr_tensor)
 
 		input_file.close()
 
+
 		if filename == test_file:
+			tree_li = pos_trees + neg_trees
 			test_trees = tree_li
-		
-		train_trees += tree_li
 	
-	model = TreeLSTM(IN_FEATURES, OUT_FEATURES).train()
+		else:			
+			tree_li = pos_trees + neg_trees
 
-	# loss_function = torch.nn.CrossEntropyLoss()
-	loss_function = torch.nn.BCEWithLogitsLoss()
+			shuffle(tree_li)
 
-	optimizer = torch.optim.Adam(model.parameters())
+			train_trees += tree_li
+	
+	model = TreeLSTM(IN_FEATURES, OUT_FEATURES, HIDDEN_UNITS).train()
+
+	loss_function = torch.nn.CrossEntropyLoss()
+
+	optimizer = torch.optim.Adam(model.parameters() , lr = LEARNING_RATE)
 
 	for i in range(NUM_ITERATIONS):
 		total_loss = 0
@@ -82,16 +108,18 @@ for test_file in test_set:
 					tree_batch['edge_order'],
 					tree_batch['root_node'],
 					tree_batch['root_label']
-				)
+					)
 			except:
 				continue
 			
 			labels = tree_batch['l']
 			root_labels = tree_batch['root_label']
 
-
 			loss = loss_function(h_root, root_labels)
+
+
 			loss.backward()
+
 			optimizer.step()
 
 			total_loss += loss
@@ -104,6 +132,9 @@ for test_file in test_set:
 
 	acc = 0
 	total = 0
+
+	pred_label_li = []
+	true_label_li = []
 
 	for test in test_trees:
 		try:
@@ -120,12 +151,19 @@ for test_file in test_set:
 
 		pred_v, pred_label = torch.max(h_test_root, 1)
 
-		true_label = test['root_l'][0][1]
+		true_label = test['root_l']
 
 		if pred_label == true_label:
 			acc += 1
 
+		pred_label_li.append(pred_label)
+		true_label_li.append(true_label)
+
 		total += 1
 
-	
+	macro_f1 = f1_score(pred_label_li, true_label_li, average = 'macro')
+
+
 	print(test_file, 'accuracy:', acc / total)
+	print(test_file, 'f1:', macro_f1)
+	print(test_file, 'total tested:', total)

treelstm/tree_lstm.py
@@ -10,7 +10,7 @@ import torch
 class TreeLSTM(torch.nn.Module):
 	'''PyTorch TreeLSTM model that implements efficient batching.
 	'''
-	def __init__(self, in_features, out_features):
+	def __init__(self, in_features, out_features, hidden_units):
 		'''TreeLSTM class initializer
 
 		Takes in int sizes of in_features and out_features and sets up model Linear network layers.
@@ -18,15 +18,30 @@ class TreeLSTM(torch.nn.Module):
 		super().__init__()
 		self.in_features = in_features
 		self.out_features = out_features
+		self.hidden_units = hidden_units
+
 
 		# bias terms are only on the W layers for efficiency
-		self.W_iou = torch.nn.Linear(self.in_features, 3 * self.out_features)
-		self.U_iou = torch.nn.Linear(self.out_features, 3 * self.out_features, bias=False)
+		self.W_iou = torch.nn.Linear(self.in_features, 3 * (self.hidden_units//16) * self.out_features)
+
+		self.U_iou = torch.nn.Linear((self.hidden_units//16) * self.out_features, 3 * (self.hidden_units//16) * self.out_features, bias=False)
 
 		# f terms are maintained seperate from the iou terms because they involve sums over child nodes
 		# while the iou terms do not
-		self.W_f = torch.nn.Linear(self.in_features, self.out_features)
-		self.U_f = torch.nn.Linear(self.out_features, self.out_features, bias=False)
+		self.W_f = torch.nn.Linear(self.in_features, (self.hidden_units//16) * self.out_features)
+		self.U_f = torch.nn.Linear((self.hidden_units//16) * self.out_features, (self.hidden_units//16) * self.out_features, bias=False)
+
+		self.feedforward = torch.nn.Linear((self.hidden_units//16) * self.out_features, self.out_features, bias=False)
+
+		self.init_weights()
+
+	def init_weights(self):
+		for p in self.parameters():
+			if p.data.ndimension() >= 2:
+				torch.nn.init.xavier_uniform_(p.data)
+			else:
+				torch.nn.init.zeros_(p.data)
+
 
 	def forward(self, features, node_order, adjacency_list, edge_order, root_node, root_label):
 		'''Run TreeLSTM model on a tree data structure with node features
@@ -42,9 +57,9 @@ class TreeLSTM(torch.nn.Module):
 		device = next(self.parameters()).device
 
 		# h and c states for every node in the batch
-		h = torch.zeros(batch_size, self.out_features, device=device)
+		h = torch.zeros(batch_size, (self.hidden_units//16) * self.out_features, device=device)
 		
-		c = torch.zeros(batch_size, self.out_features, device=device)
+		c = torch.zeros(batch_size, (self.hidden_units//16) * self.out_features, device=device)
 
 		# print(node_order.shape)
 
@@ -53,7 +68,7 @@ class TreeLSTM(torch.nn.Module):
 			# print(n)
 			self._run_lstm(n, h, c, features, node_order, adjacency_list, edge_order)
 
-		h_root = h[root_node, :]
+		h_root = self.feedforward(h[root_node, :])
 
 		h_root = torch.nn.functional.softmax(h_root, dim = 1)
 
@@ -79,12 +94,13 @@ class TreeLSTM(torch.nn.Module):
 
 		# print(node_mask)
 
-
 		# edge_mask is a tensor of size E x 1
 		edge_mask = edge_order == iteration
 
 		# x is a tensor of size n x F
 		x = features[node_mask, :]
+
+		# x = self.hidden_layer(x_orig)
 
 		# At iteration 0 none of the nodes should have children
 		# Otherwise, select the child nodes needed for current iteration

treelstm/util.py
@@ -49,13 +49,11 @@ def convert_tree_to_tensors(tree, device=torch.device('cpu')):
 	features = _gather_node_attributes(tree, 'f')
 	labels = _gather_node_attributes(tree, 'l')
 
-	root_label = [labels[0]]
+	root_label = labels[0]
 
 	# print(root_label)
 
 	adjacency_list = _gather_adjacency_list(tree)
-
-	# print(adjacency_list)
 
 	node_order, edge_order = calculate_evaluation_orders(adjacency_list, len(features))
 
@@ -64,12 +62,12 @@ def convert_tree_to_tensors(tree, device=torch.device('cpu')):
 	return {
 		'f': torch.tensor(features, device=device, dtype=torch.float32),
 		'l': torch.tensor(labels, device=device, dtype=torch.float32),
-		'root_l': torch.tensor(root_label, device=device, dtype=torch.float32),
+		'root_l': torch.tensor(root_label, device=device, dtype=torch.long),
 		'root_n': torch.tensor(root_node, device=device, dtype=torch.int64),
 		'node_order': torch.tensor(node_order, device=device, dtype=torch.int64),
 		'adjacency_list': torch.tensor(adjacency_list, device=device, dtype=torch.int64),
 		'edge_order': torch.tensor(edge_order, device=device, dtype=torch.int64),
-	}
+	}, root_label
 
 def calculate_evaluation_orders(adjacency_list, tree_size):
     '''Calculates the node_order and edge_order from a tree adjacency_list and the tree_size.
@@ -84,6 +82,8 @@ def calculate_evaluation_orders(adjacency_list, tree_size):
 
     node_order = numpy.zeros(tree_size, dtype=int)
     unevaluated_nodes = numpy.ones(tree_size, dtype=bool)
+
+    # print(adjacency_list)
 
     parent_nodes = adjacency_list[:, 0]
     child_nodes = adjacency_list[:, 1]
@@ -134,7 +134,7 @@ def batch_tree_input(batch):
 
     batched_labels = torch.cat([b['l'] for b in batch])
 
-    batched_root_labels = torch.cat([b['root_l'] for b in batch])
+    batched_root_labels = torch.tensor([b['root_l'] for b in batch])
 
     batched_adjacency_list = []
     offset = 0